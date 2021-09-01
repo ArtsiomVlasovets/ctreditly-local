ac3ee5e2ae1eee23df44c73410300390a981c062
zachswdream <umarahmaddh@gmail.com>
2021-09-01 15:27:43 +0500

    animation

--- src/app/creditly-b2b/animation-section/animation-section.component.ts
+++ src/app/creditly-b2b/animation-section/animation-section.component.ts
@@ -10,12 +10,13 @@ gsap.registerPlugin(ScrollTrigger);
   styleUrls: ['./animation-section.component.scss']
 })
 export class AnimationSectionComponent implements OnInit {
-	scriptAnimArr = [
-	  '/assets/gsap.min.js',
-	  '/assets/ScrollTrigger.min.js',
-    // 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.0/gsap.min.js',
-    // 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollTrigger.min.js'
-  ];
+
+// 	scriptAnimArr = [
+// 	  '/assets/gsap.min.js',
+// 	  '/assets/ScrollTrigger.min.js',
+//     // 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.0/gsap.min.js',
+//     // 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollTrigger.min.js'
+//   ];
     @ViewChild('animation_wrapper', { static: true }) animWrapper: ElementRef<HTMLDivElement>;
   // animWrapper = document.getElementById("animation_wrapper")
   animId;
@@ -3061,13 +3062,13 @@ gsapAnim()
     scriptList.classList.add('containerScriptList')
     this.animWrapper.nativeElement.appendChild(scriptList)
 
-    this.scriptAnimArr.forEach((elem)=>{
-        let tempScipt = document.createElement('script')
-		tempScipt.src = elem;
-		// tempScipt.nonce = "r@nd0m";
+    // this.scriptAnimArr.forEach((elem)=>{
+    //     let tempScipt = document.createElement('script')
+	// 	tempScipt.src = elem;
+	// 	// tempScipt.nonce = "r@nd0m";
 		
-        scriptList.appendChild(tempScipt)
-    })
+    //     scriptList.appendChild(tempScipt)
+    // })
  }
   
   
